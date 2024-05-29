994f6ef7f56cd097f9828f1f777255e4f63ab95a
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-05-29 22:32:28 +0700

    refactor: tái cấu trúc code file app để test component trước

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,7 @@
-import { useState } from 'preact/hooks'
-import viteLogo from '../public/vite.svg'
-import './app.css'
-import pReactLogo from './assets/preact.svg'
+import NotFoundPage from './components/FallbackPages/NotFoundPage'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={pReactLogo} className="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <NotFoundPage />
   )
 }
